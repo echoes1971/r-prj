442f787f33b35bff42cdae9c0e15ed2ec8b45a44
roberto swisscom <roberto.roccoangeloni@gmail.com>
2015-08-10 15:44:03 +0200

    updated sloc cost :-)

--- sloc.sh
+++ sloc.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+COST_PER_YEAR=97200
+
 # External Libraries
-sloccount --personcost 37200 php/formulator/jscalendar-1.0 php/js/3rdparties php/xmlrpc php/plugins/tinymce/tinymce php/plugins/tinymce/tinymce_3.4.7.zip php/plugins/tinymce/skins/default/jscripts
+sloccount --personcost $COST_PER_YEAR php/formulator/jscalendar-1.0 php/js/3rdparties php/xmlrpc php/plugins/tinymce/tinymce php/plugins/tinymce/tinymce_3.4.7.zip php/plugins/tinymce/skins/default/jscripts
 
 
 # My code
-sloccount --personcost 37200 php python
+sloccount --personcost $COST_PER_YEAR php python
 
