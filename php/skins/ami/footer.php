b19b7700c0efc732bd3d62e4d58ae6cc00208eaf
roberto swisscom <roberto.roccoangeloni@gmail.com>
2015-12-08 15:57:49 +0100

    started rnotes skin (ami)

---
+++ php/skins/ami/footer.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @copyright &copy; 2015 by Roberto Rocco Angeloni <roberto@roccoangeloni.it>
+ * @license http://opensource.org/licenses/lgpl-3.0.html GNU Lesser General Public License, version 3.0 (LGPLv3)
+ * @version $Id: footer.php $
+ * @package rproject
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+do_hook('footer_before');
+?><div id="footer"><hr class="amiga" /><?php
+do_hook('footer_content_before');
+?><small><b>&copy; 2005-<?php echo date("Y"); ?> by Roberto Rocco Angeloni - All rights reserved. - Powered by <a href="http://u.bb/77j">R-Project</a>.</b></small><?php
+do_hook('footer_content_after');
+?></div><?php
+do_hook('footer_after');
+
+
+?></body>
+</html>
