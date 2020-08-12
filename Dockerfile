df6faf7774e9151367d984e23bbf69097ef3efc4
Roberto R.A <rrocco@swissbilling.ch>
2020-08-12 15:05:05 +0200

    added RPRJ_ADMIN_PASS

--- Dockerfile
+++ Dockerfile
@@ -4,12 +4,10 @@ ENV SITE_TITLE My Site
 ENV MYSQL_APP rprj-db
 ENV MYSQL_DB rproject
 ENV MYSQL_PASSWORD mysecret
+ENV RPRJ_ADMIN_PASS myrprjsecret
 
 RUN apt-get update -y
 RUN apt-get install -y zlib1g-dev libzip-dev
-
-# Install mysqli
-RUN docker-php-ext-install mysqli && docker-php-ext-enable mysqli
 
 # See: https://stackoverflow.com/questions/39657058/installing-gd-in-docker
 RUN apt-get install -y apt-utils \
@@ -19,19 +17,19 @@ RUN apt-get install -y apt-utils \
 # Already loaded?
 # RUN docker-php-ext-install mbstring
 
+# Install mysqli
+RUN docker-php-ext-install mysqli
+# RUN docker-php-ext-install mysqli && docker-php-ext-enable mysqli
+
 RUN docker-php-ext-install zip
 
-RUN docker-php-ext-configure gd
-# RUN docker-php-ext-configure gd --with-gd --with-webp-dir --with-jpeg-dir \
-#     --with-png-dir --with-zlib-dir --with-xpm-dir --with-freetype-dir \
-#     --enable-gd-native-ttf
-
+RUN docker-php-ext-configure gd --enable-gd --with-webp --with-jpeg --with-xpm --with-freetype
 RUN docker-php-ext-install gd
 
 # Copy the source
 COPY php/ /var/www/html/
 
-COPY webentrypoint.sh /usr/local/bin/
+COPY docker/webentrypoint.sh /usr/local/bin/
 COPY docker/webentrypoint.sh /usr/local/bin/docker-php-entrypoint
 
 # ENTRYPOINT /usr/local/bin/webentrypoint.sh
--- docker/webentrypoint.sh
+++ docker/webentrypoint.sh
@@ -6,12 +6,19 @@ if [ "${1#-}" != "$1" ]; then
   set -- apache2-foreground "$@"
 fi
 
-echo "  ___     ___     _ "
-echo " | _ \___| _ \_ _(_)"
-echo " |   /___|  _/ '_| |"
-echo " |_|_\   |_| |_|_/ |"
-echo "               |__/ "
-
+# echo "  ___     ___     _ "
+# echo " | _ \___| _ \_ _(_)"
+# echo " |   /___|  _/ '_| |"
+# echo " |_|_\   |_| |_|_/ |"
+# echo "               |__/ "
+echo "  ___     ___          _        _   "
+echo " | _ \___| _ \_ _ ___ (_)___ __| |_ "
+echo " |   /___|  _/ '_/ _ \| / -_) _|  _|"
+echo " |_|_\   |_| |_| \___// \___\__|\__|"
+echo "                    |__/            "
+echo
+echo "(C) 2005-2020 by Roberto Rocco-Angeloni"
+echo
 # echo "Environment:"
 # echo "$SITE_TITLE"
 # echo "$MYSQL_APP"
@@ -24,7 +31,12 @@ sed -i s/rprj-mariadb/$MYSQL_APP/g /var/www/html/config_local.php
 sed -i s/rproject/$MYSQL_DB/g /var/www/html/config_local.php
 sed -i s/mysecret/$MYSQL_PASSWORD/g /var/www/html/config_local.php
 # sed -i s/setVerbose\(false\)/setVerbose\(true\)/g /var/www/html/mng/db_update_do.php
+sed -i s/adm\',\'adm\'/adm\',\'$RPRJ_ADMIN_PASS\'/g /var/www/html/mng/db_update_do.php
 
-# cd /var/www/html/mng/ ; php /var/www/html/mng/db_update_do.php # > /dev/null 2>&1
+cd /var/www/html/mng/ ; php /var/www/html/mng/docker_waitdb.php # > /dev/null 2>&1
+cd /var/www/html/mng/ ; php /var/www/html/mng/db_update_do.php > /var/log/webentrypoint.log 2>&1
+
+chmod 777 /var/www/html/files
+chmod 777 /var/www/html/mng/files
 
 exec "$@"
