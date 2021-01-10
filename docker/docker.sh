13910c0e164667fcabb5905d3342a1fcadc5f25f
Mr. Echoes <mr.echoes@gmail.com>
2021-01-10 18:27:33 +0100

    split dockerfile

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 PRJ_HOME=`cd ..; pwd`
+RPRJ_IMG_BASE=rprj-php-base
 RPRJ_IMG=rprj-mariadb-image
 PHP_APP=rprj-php-mariadb
 MYSQL_APP=rprj-mariadb
 MYSQL_DB=rproject
 MYSQL_PASSWORD=mysecret
 
-if [ "$1" = "clean" ]; then
+if [ "$1" = "clean" ] || [ "$1" = "cleanall" ]; then
  echo "Stopping containers..."
  docker container stop $PHP_APP
  docker container stop $MYSQL_APP
@@ -15,6 +16,9 @@ if [ "$1" = "clean" ]; then
  docker container rm $PHP_APP
  docker container rm $MYSQL_APP
  docker image rm $RPRJ_IMG
+ if [ "$1" = "cleanall" ]; then
+  docker image rm $RPRJ_IMG_BASE
+ fi
  echo
 #  docker container ls -a
 #  docker image ls -a
@@ -39,7 +43,7 @@ if [ -z "$MYSQL_EXISTS" ]; then
   -d mariadb:10.3
  #echo "Initialize DB with: docker exec -it $MYSQL_APP mysql -p$MYSQL_PASSWORD -e \"create database $MYSQL_DB;\""
 
- echo -n "Creating DB"
+ echo -n "Creating DB..."
  docker exec -it $MYSQL_APP mysql -p$MYSQL_PASSWORD -e "create database if not exists $MYSQL_DB;" > /dev/null 2&>1
  retVal=$?
  while [ $retVal -ne 0 ]; do
@@ -56,12 +60,24 @@ fi
 rm -rf build
 mkdir build
 cp -R ../php/* ./build/
-# Config_local
-cp ../php/config_local.sample.php ./build/config_local.php
-sed -i s/rprj-db-server/$MYSQL_APP/g ./build/config_local.php
-sed -i s/rprj-db-db/$MYSQL_DB/g ./build/config_local.php
-sed -i s/rprj-db-pwd/$MYSQL_PASSWORD/g ./build/config_local.php
-#sed -i s/setVerbose\(false\)/setVerbose\(true\)/g ./build/mng/db_update_do.php
+
+# # Config_local
+# cp ../php/config_local.sample.php ./build/config_local.php
+# sed -i s/rprj-db-server/$MYSQL_APP/g ./build/config_local.php
+# sed -i s/rprj-db-db/$MYSQL_DB/g ./build/config_local.php
+# sed -i s/rprj-db-pwd/$MYSQL_PASSWORD/g ./build/config_local.php
+# #sed -i s/setVerbose\(false\)/setVerbose\(true\)/g ./build/mng/db_update_do.php
+
+IMG_BASE_EXISTS=`docker image ls | grep $RPRJ_IMG_BASE`
+#echo $IMG_BASE_EXISTS
+# See: http://timmurphy.org/2010/05/19/checking-for-empty-string-in-bash/
+if [ -n "$IMG_BASE_EXISTS" ]; then
+ echo "* Image $RPRJ_IMG_BASE exists"
+fi
+if [ -z "$IMG_BASE_EXISTS" ]; then
+ echo "* Creating image $RPRJ_IMG_BASE"
+ docker build -f ./Dockerfile_base -t $RPRJ_IMG_BASE .
+fi
 
 IMG_EXISTS=`docker image ls | grep $RPRJ_IMG`
 #echo $IMG_EXISTS
@@ -85,12 +101,16 @@ fi
 if [ -z "$PHP_EXISTS" ]; then
  echo "* Creating container $PHP_APP"
  docker run -p 8080:80 --name $PHP_APP \
+ -e MYSQL_APP=$MYSQL_APP \
+ -e MYSQL_DB=$MYSQL_DB \
+ -e MYSQL_PASSWORD=$MYSQL_PASSWORD \
  -v "$PRJ_HOME/files":/var/www/html/files \
  -v "$PRJ_HOME/files":/var/www/html/mng/files \
  --link $MYSQL_APP:mysql \
  -d $RPRJ_IMG
 
  # Init DB
+ echo -n "Init DB..."
  docker exec -it $PHP_APP bash -c "cd /var/www/html/mng/ ; php db_update_do.php" > /dev/null
  retVal=$?
  while [ $retVal -ne 0 ]; do
@@ -99,7 +119,8 @@ if [ -z "$PHP_EXISTS" ]; then
   docker exec -it $PHP_APP bash -c "cd /var/www/html/mng/ ; php db_update_do.php" > /dev/null
   retVal=$?
  done
- docker exec -it $MYSQL_APP mysql -p$MYSQL_PASSWORD -e "show tables;" $MYSQL_DB
+ echo " done!"
+ #docker exec -it $MYSQL_APP mysql -p$MYSQL_PASSWORD -e "show tables;" $MYSQL_DB
 fi
 
 
