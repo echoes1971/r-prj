36a343466cc9abc0205b29e7363f45ff191425e5
Roberto Rocco-Angeloni <roberto.rocco@nagra.com>
2022-03-10 15:56:51 +0100

    removed white spaces

--- php/mng/dbe_delete_do.php
+++ php/mng/dbe_delete_do.php
@@ -31,14 +31,14 @@ require_once(ROOT_FOLDER . "plugins.php");
 $redir_page = "dbe_list.php";
 
 $dbmgr = $_SESSION['dbmgr'];
-if ($dbmgr==NULL || get_class($dbmgr)=='__PHP_Incomplete_Class') {
+if($dbmgr==NULL || get_class($dbmgr)=='__PHP_Incomplete_Class') {
 	$aFactory = new MyDBEFactory;
 	$dbmgr = new ObjectMgr( $db_server, $db_user, $db_pwd, $db_db, $db_schema, $aFactory );
 	$_SESSION['dbmgr'] = $dbmgr;
 }
 $dbmgr->setVerbose(false);
 $formulator = array_key_exists('formulator',$_SESSION) ?$_SESSION['formulator'] : null;
-if ($formulator==null || get_class($formulator)=='__PHP_Incomplete_Class') {
+if($formulator==null || get_class($formulator)=='__PHP_Incomplete_Class') {
 	$formulator = new MyFormFactory;
 	$_SESSION['formulator'] = $formulator;
 }
