76e891988f1e47fba6325a8267800141238f0add
Mr. Echoes <mr.echoes@gmail.com>
2022-03-27 18:52:35 +0200

    refactoring

--- rprj-app/src/comp.fform.js
+++ rprj-app/src/comp.fform.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { FPermissions } from './comp.fpermissions';
 import { BackEndProxy } from './be';
 
 class FForm extends React.Component {
@@ -27,7 +28,6 @@ class FForm extends React.Component {
         this.default_handleSubmit = this.default_handleSubmit.bind(this);
         this.default_handleChange = this.default_handleChange.bind(this);
         this.password_handleChange = this.password_handleChange.bind(this);
-        this.permissions_handleChange = this.permissions_handleChange.bind(this);
 
         // this.render = this.render.bind(this);
         this.renderGroups = this.renderGroups.bind(this);
@@ -245,125 +245,6 @@ class FForm extends React.Component {
             </div>
         );
     }
-    permissions_handleChange(event) {
-        const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
-        const name = target.name;
-
-        const fieldname = name.substring(5,name.length);
-
-        const permission_array = [
-            this.state['btn0_'+fieldname], this.state['btn1_'+fieldname], this.state['btn2_'+fieldname],
-            this.state['btn3_'+fieldname], this.state['btn4_'+fieldname], this.state['btn5_'+fieldname],
-            this.state['btn6_'+fieldname], this.state['btn7_'+fieldname], this.state['btn8_'+fieldname]
-        ];
-        const myindex = name.substr(3,1)
-        permission_array[parseInt(myindex)] = value
-
-        const permissions = ''
-                        + (permission_array[0] ? 'r' : '-')
-                        + (permission_array[1] ? 'w' : '-')
-                        + (permission_array[2] ? 'x' : '-')
-                        + (permission_array[3] ? 'r' : '-')
-                        + (permission_array[4] ? 'w' : '-')
-                        + (permission_array[5] ? 'x' : '-')
-                        + (permission_array[6] ? 'r' : '-')
-                        + (permission_array[7] ? 'w' : '-')
-                        + (permission_array[8] ? 'x' : '-');
-
-        const state_fieldname = this.field_prefix + target.name.substring(5);
-        this.setState({[name]: value, [state_fieldname]:permissions});
-    }
-    renderFPermissions(field, is_readonly=false) {
-        const fieldname = field.name;
-        const fieldclass = (
-                (field.cssClass>'' ? field.cssClass : '') + ' '
-                + (is_readonly ? 'form-control-plaintext' : '')
-            ).trim();
-        const value = this.state[fieldname] || '---------'; //'r-x-w-rw-';
-        return (
-            <div class="row">
-                <div class="col-1 text-end d-none d-lg-block">{field.title}</div>
-                <div class="col text-start">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-2">
-                                <div class="row"><div class="col fw-bold text-center">User</div></div>
-                                <div class="row">
-                                    <div class="col">
-                                        <div class="btn-group btn-group-sm" role="group" aria-label="User">
-                                            <input type="checkbox" class="btn-check"
-                                                id={'btn0_'+fieldname} name={'btn0_'+fieldname} autocomplete="off"
-                                                defaultChecked={value[0]=='r'} onChange={this.permissions_handleChange} />
-                                            <label class="btn btn-outline-primary" for={'btn0_'+fieldname}>Read</label>
-
-                                            <input type="checkbox" class="btn-check"
-                                                id={'btn1_'+fieldname} name={'btn1_'+fieldname} autocomplete="off"
-                                                defaultChecked={value[1]=='w'} onChange={this.permissions_handleChange} />
-                                            <label class="btn btn-outline-primary" for={'btn1_'+fieldname}>Write</label>
-
-                                            <input type="checkbox" class="btn-check"
-                                                id={'btn2_'+fieldname} name={'btn2_'+fieldname} autocomplete="off"
-                                                defaultChecked={value[2]=='x'} onChange={this.permissions_handleChange} />
-                                            <label class="btn btn-outline-primary" for={'btn2_'+fieldname}>Execute</label>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="col-2">
-                                <div class="row"><div class="col fw-bold text-center">Group</div></div>
-                                <div class="row">
-                                    <div class="col">
-                                        <div class="btn-group btn-group-sm" role="group" aria-label="Group">
-                                            <input type="checkbox" class="btn-check"
-                                                id={'btn3_'+fieldname} name={'btn3_'+fieldname} autocomplete="off"
-                                                defaultChecked={value[3]=='r'} onChange={this.permissions_handleChange} />
-                                            <label class="btn btn-outline-primary" for={'btn3_'+fieldname}>Read</label>
-
-                                            <input type="checkbox" class="btn-check"
-                                                id={'btn4_'+fieldname} name={'btn4_'+fieldname} autocomplete="off"
-                                                defaultChecked={value[4]=='w'} onChange={this.permissions_handleChange} />
-                                            <label class="btn btn-outline-primary" for={'btn4_'+fieldname}>Write</label>
-
-                                            <input type="checkbox" class="btn-check"
-                                                id={'btn5_'+fieldname} name={'btn5_'+fieldname} autocomplete="off"
-                                                defaultChecked={value[5]=='x'} onChange={this.permissions_handleChange} />
-                                            <label class="btn btn-outline-primary" for={'btn5_'+fieldname}>Execute</label>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="col-2">
-                                <div class="row"><div class="col fw-bold text-center">All</div></div>
-                                <div class="row">
-                                    <div class="col">
-                                        <div class="btn-group btn-group-sm" role="group" aria-label="All">
-                                            <input type="checkbox" class="btn-check"
-                                                id={'btn6_'+fieldname} name={'btn6_'+fieldname} autocomplete="off"
-                                                defaultChecked={value[6]=='r'} onChange={this.permissions_handleChange} />
-                                            <label class="btn btn-outline-primary" for={'btn6_'+fieldname}>Read</label>
-
-                                            <input type="checkbox" class="btn-check"
-                                                id={'btn7_'+fieldname} name={'btn7_'+fieldname} autocomplete="off"
-                                                defaultChecked={value[7]=='w'} onChange={this.permissions_handleChange} />
-                                            <label class="btn btn-outline-primary" for={'btn7_'+fieldname}>Write</label>
-
-                                            <input type="checkbox" class="btn-check"
-                                                id={'btn8_'+fieldname} name={'btn8_'+fieldname} autocomplete="off"
-                                                defaultChecked={value[8]=='x'} onChange={this.permissions_handleChange} />
-                                            <label class="btn btn-outline-primary" for={'btn8_'+fieldname}>Execute</label>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
     renderFTextArea(field, is_readonly=false) {
         const fieldname = this.field_prefix + field.name
         const fieldclass = (
@@ -402,7 +283,11 @@ class FForm extends React.Component {
             return this.renderFPercent(field, is_readonly);
         }
         if(field._classname==='FPermissions') {
-            return this.renderFPermissions(field,is_readonly);
+            return (
+                <FPermissions name={field.name} title={field.title} cssClass={this.cssClass}
+                    is_readonly={is_readonly} field_prefix={this.field_prefix}
+                    onChange={this.default_handleChange} />
+            );
         }
         if(field._classname==='FTextArea') {
             return this.renderFTextArea(field,is_readonly)
