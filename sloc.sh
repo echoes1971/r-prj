8270e700c6b8002a62cfa0a1f16d500a21ba7924
Mr. Echoes <mr.echoes@gmail.com>
2022-03-27 16:01:31 +0200

    added new directories

--- sloc.sh
+++ sloc.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-COST_PER_YEAR=97200
+COST_PER_YEAR=100000
 
 # External Libraries
 sloccount --personcost $COST_PER_YEAR php/formulator/jscalendar-1.0 php/js/3rdparties php/xmlrpc php/plugins/tinymce/tinymce php/plugins/tinymce/tinymce_3.4.7.zip php/plugins/tinymce/skins/default/jscripts
 
 
 # My code
-sloccount --personcost $COST_PER_YEAR php python
+sloccount --personcost $COST_PER_YEAR ansible docker k8s php python rprj-app
 
