f5ec74010fdbebef8bba4b077d5b910297cb3ef8
Mr. Echoes <mr.echoes@gmail.com>
2022-03-28 19:23:37 +0200

    Profile menu

--- rprj-app/src/App.js
+++ rprj-app/src/App.js
@@ -16,25 +16,77 @@ class App extends Component {
 
     // See: https://it.reactjs.org/
     this.state = {
-      endpoint: "http://localhost:8080/jsonserver.php"
+      endpoint: "http://localhost:8080/jsonserver.php",
+
+      user_fullname: ''
     };
 
     this.be = new BackEndProxy(this.state.endpoint);
 
     // Init stuff
-    this.be.ping()
-    this.be.getLoggedUser()
+    this.be.ping();
+    this.be.getLoggedUser();
+
+    this.default_callback = this.default_callback.bind(this);
+
+    this.on_login_callback = this.on_login_callback.bind(this);
+    this.onLogin = this.onLogin.bind(this);
+
+    this.on_logout_callback = this.on_logout_callback.bind(this);
+    this.onLogout = this.onLogout.bind(this);
+
+    this.on_fetchuser_callback = this.on_fetchuser_callback.bind(this);
+    this.fetchLoggedUser = this.fetchLoggedUser.bind(this);
   }
 
   componentDidMount() {
+    this.fetchLoggedUser();
+    this.pingID = setInterval(
+        // () => this.be.ping(this.on_ping_callback),
+        () => this.fetchLoggedUser(),
+        20 * 1000                   // Better 60 seconds?
+    );
+}
+
+  default_callback(jsonObj) {
+    // console.log("App.on_btn_ping_callback: start.");
+    // console.log(jsonObj)
+    // console.log(this.be.isConnected())
+    // console.log("App.on_btn_ping_callback: end.");
+  }
+
+  on_fetchuser_callback(jsonObj) {
+    const tmpUser = this.be.getDBEUserFromConnection();
+    this.setState({
+        user: tmpUser ? tmpUser.getValue('fullname') + " " + tmpUser.to_string() : ''
+    })
+  }
+  fetchLoggedUser() {
+    this.be.getLoggedUser(this.on_fetchuser_callback);
+  }
+
+  on_login_callback(jsonObj) {
+    const tmpUser = this.be.getDBEUserFromConnection();
+    this.setState({
+        user_fullname: tmpUser ? tmpUser.getValue('fullname') : ''
+    })
+  }
+  onLogin(usr,pwd) {
+    this.be.login(usr,pwd,this.on_login_callback);
+  }
+
+  on_logout_callback(jsonObj) {
+    this.setState({user_fullname: ''})
+  }
+  onLogout() {
+    this.be.logout(this.on_logout_callback);
   }
 
   render() {
-    const dbeuser = this.be.getDBEUserFromConnection();
-    const user_fullname = dbeuser!==null && dbeuser!==undefined ? dbeuser.getValue("fullname") : null;
     return (
       <div className="App">
-        <RNav user_fullname={user_fullname} />
+        <RNav user_fullname={this.state.user_fullname}
+          onLogin={this.onLogin} onLogout={this.onLogout} />
         <div class="container">
           <TestBE endpoint={this.state.endpoint} ref={this.myRef} />
         </div>
--- rprj-app/src/comp.nav.js
+++ rprj-app/src/comp.nav.js
@@ -8,15 +8,31 @@ class RNav extends React.Component {
         // console.log(props);
         
         this.state = {
-            usr: '',
-            pwd: ''
+            usr: ''
+            ,pwd: ''
+            ,user_fullname: props.user_fullname
         }
 
         // this.be = new BackEndProxy(this.state.endpoint);
         this.renderProfile = this.renderProfile.bind(this);
 
-        this.default_handleSubmit = this.default_handleSubmit.bind(this);
         this.default_handleChange = this.default_handleChange.bind(this);
+        this.default_handleSubmit = this.default_handleSubmit.bind(this);
+
+        this.logout_handleSubmit = this.logout_handleSubmit.bind(this);
+
+        this.btnLogin = this.btnLogin.bind(this);
+        this.btnLogout = this.btnLogout.bind(this);
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        // console.log("FForm.componentDidUpdate: prevProps="+JSON.stringify(prevProps))
+        // console.log("FForm.componentDidUpdate: prevState="+JSON.stringify(prevState))
+        // console.log("FForm.componentDidUpdate: props="+JSON.stringify(this.props))
+        // console.log("FForm.componentDidUpdate: state="+JSON.stringify(this.state))
+        if(this.props.user_fullname !== prevProps.user_fullname) {
+            this.setState({user_fullname: this.props.user_fullname})
+        }
     }
 
     default_handleSubmit(event) {
@@ -29,11 +45,43 @@ class RNav extends React.Component {
 
         this.setState({[name]: value});
     }
+    login_handleSubmit(event) {
+        event.preventDefault();
+    }
+    btnLogin() {
+        const usr = this.state.usr;
+        const pwd = this.state.pwd;
+        this.props.onLogin(usr,pwd);
+    }
+    logout_handleSubmit(event) {
+        event.preventDefault();
+    }
+    btnLogout() {
+        this.props.onLogout();
+    }
 
     renderProfile(user_fullname) {
         if(user_fullname) {
             return (
-                <h1>{user_fullname}</h1>
+                <div class="nav-item dropdown dropdown-menu-right">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item  dropdown">
+
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarProfileDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            {user_fullname}
+                            </a>
+
+                            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarProfileDropdown">
+                                <li class="dropdown-item">
+                                    <form onSubmit={this.default_handleSubmit} >
+                                        <button class="btn btn-secondary" onClick={this.btnLogout}>Logout</button>
+                                    </form>
+                                </li>
+                            </ul>
+
+                        </li>
+                    </ul>
+                </div>
             );
         }
         return (
@@ -41,19 +89,19 @@ class RNav extends React.Component {
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item  dropdown">
 
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarLoginDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Login
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarLoginDropdown">
-                        <li class="dropdown-item">
-                            <form onSubmit={this.default_handleSubmit} >
-                                <label class="d-none d-lg-block" for="usr">Username</label> <input id="usr" name="usr" value={this.state.usr} onChange={this.default_handleChange} placeholder="Username"/>
-                                <br />
-                                <label class="d-none d-lg-block" for="usr">Password</label> <input id="pwd" name="pwd" type="password" value={this.state.pwd} onChange={this.default_handleChange} placeholder="Password" /> <br />
-                                <button class="btn btn-secondary" onClick={this.btnLogin}>Login</button>
-                            </form>
-                        </li>
-                    </ul>
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarLoginDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Login
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarLoginDropdown">
+                            <li class="dropdown-item">
+                                <form onSubmit={this.default_handleSubmit} >
+                                    <label class="d-none d-lg-block" for="usr">Username</label> <input id="usr" name="usr" value={this.state.usr} onChange={this.default_handleChange} placeholder="Username"/>
+                                    <br />
+                                    <label class="d-none d-lg-block" for="usr">Password</label> <input id="pwd" name="pwd" type="password" value={this.state.pwd} onChange={this.default_handleChange} placeholder="Password" /> <br />
+                                    <button class="btn btn-secondary" onClick={this.btnLogin}>Login</button>
+                                </form>
+                            </li>
+                        </ul>
 
                     </li>
                 </ul>
@@ -62,7 +110,7 @@ class RNav extends React.Component {
     }
 
     render() {
-        const user_fullname = this.props.user_fullname || "";
+        //  &#129489;
         return (
             <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
                 <a class="navbar-brand d-none d-lg-block" href="#">R-Prj</a>
@@ -100,19 +148,16 @@ class RNav extends React.Component {
                                     <button class="btn btn-outline-success" type="submit">Search</button>
                                 </form>
                             </li>
-
                         </ul>
-
-
                     </div>
 
                     <a class="navbar-brand d-block d-lg-none" href="#">R-Prj</a>
 
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarProfileContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    {user_fullname ? ( <span>&#129333;</span> ) : ( <span>&#128100;</span> ) } &#129489;
+                    {this.state.user_fullname ? ( <span>&#129333;</span> ) : ( <span>&#128100;</span> ) }
                     </button>
                     <div class="collapse navbar-collapse justify-content-end" id="navbarProfileContent">
-                    { this.renderProfile(user_fullname) }
+                    { this.renderProfile(this.state.user_fullname ) }
                     </div>
                 </div>
             </nav>
